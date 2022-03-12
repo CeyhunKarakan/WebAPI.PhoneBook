a04047b7c9f331b3539d3c41ad9826aa088574bc
CeyhunKarakan <karakanceyhun@hotmail.com>
2022-03-12 09:45:05 +0300

    [DEV-UP]: Add data to User using with Context

--- WebAPI.PhoneBook/Data/UserContext.cs
+++ WebAPI.PhoneBook/Data/UserContext.cs
@@ -12,6 +12,14 @@ namespace WebAPI.PhoneBook.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User[]{
+            new() {UUID=1, Name="John", Surname="Doe", Company="Amazon", Phone="111444", Mail="joh.doe@amazon.com",Location="USA"},
+            new() {UUID=2, Name="Ricky", Surname="More", Company="Amazon", Phone="777888", Mail="ricky.more@amazon.com",Location="USA"}
+            });
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<User> Users { get; set; }
     }
 }
