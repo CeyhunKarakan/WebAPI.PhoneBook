744fdf6560bfda24a5632e389e13d3fb257f8b5d
CeyhunKarakan <karakanceyhun@hotmail.com>
2022-03-12 23:08:13 +0300

    [DEV-UP]: EnableCors on UserInformationsController

--- WebAPI.PhoneBook/Controllers/UsersInformationsController.cs
+++ WebAPI.PhoneBook/Controllers/UsersInformationsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using WebAPI.PhoneBook.Interfaces;
 
 namespace WebAPI.PhoneBook.Controllers
 {
+    [EnableCors]
     [ApiController]
     [Route("api/[controller]")]
     public class UsersInformationsController : ControllerBase
