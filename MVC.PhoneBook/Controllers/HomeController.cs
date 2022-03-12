91a24bb8e9f94de641db539a70238f11257e06aa
CeyhunKarakan <karakanceyhun@hotmail.com>
2022-03-12 15:44:24 +0300

    [DEV]: Create, Update, Index pages completed: Backend & Frontend

--- MVC.PhoneBook/Controllers/HomeController.cs
+++ MVC.PhoneBook/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MVC.PhoneBook.Controllers
@@ -33,7 +34,58 @@ namespace MVC.PhoneBook.Controllers
             {
                 return View(null);
             }
-           
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(UserResponseModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5000/api/users",content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["errorMessage"] = $"There is an error. Error code: {(int)responseMessage.StatusCode}";
+                return View(model);
+            }
+        }
+        public async Task<IActionResult> Update(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5000/api/users/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<UserResponseModel>(jsonData);
+                return View(data);
+            }
+            else
+            {
+                return View(null);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UserResponseModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"http://localhost:5000/api/users", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
         }
     }
 }
